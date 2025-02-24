e822db47a78a8c0214648c07edf0dc536ef7a4ab
Jacob <jacobharden3@gmail.com>
2025-02-24 19:22:51 +0000

    various fixes

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import alpacaConnection as ac
 
 def main():
     logging.basicConfig(stream=sys.stdout, level=logging.INFO)
-    config = yaml.safe_load(open("API.yaml"))
+    config = yaml.safe_load(open("api.yaml"))
     connection = ac.getStreamingConnection(config['Key'], config['Secret'])
     ac.subscribeToStream(connection, 'AAPL')
 
