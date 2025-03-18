9b43718406bf64156d5f5bcde79ed8a978b17400
Jacob <jacobharden3@gmail.com>
2025-03-18 17:51:59 +0000

    added json serialization for handling timestamp, pubsub and bq schema format.

--- alpacaConnection.py
+++ alpacaConnection.py
@@ -6,15 +6,17 @@ import pubsubHandler as ps
 logger = logging.getLogger(__name__)
 
 def getStreamingConnection(key, secret):
-    conn = StockDataStream(key, secret, DataFeed.IEX)
+    conn = StockDataStream(key, secret, True, DataFeed.IEX) #sets up streaming connection with alpaca API
     logger.info("Establishing connection with alpaca API")
     return conn
 
-async def handle_trade(data):
+async def handle_trade(data): #async function to handle incoming trade data and publish it to Pub/Sub
     logger.info(f"data is {data}")
-    ps.publishMessage(data)
+    jsonData = ps.serializeJSON(data)
+    print(jsonData)
+    ps.publishMessage(jsonData)
 
-def subscribeToStream(client, symbols):
+def subscribeToStream(client, symbols): #subscribes to a stream of given symbols
     client.subscribe_bars(handle_trade, symbols)
     logger.info(f"Subscribed to {symbols}")
     client.run()
--- pubsubHandler.py
+++ pubsubHandler.py
@@ -1,12 +1,14 @@
 from google.cloud import pubsub
 import logging
 import yaml
+import json
+import re
 
 
 logger = logging.getLogger(__name__)
 pubSubConfig = yaml.safe_load(open("config.yaml"))
 
-def publishMessage(message):
+def publishMessage(message):    #publishes message to pubsub
     publisher = pubsub.PublisherClient()
     psAddress = 'projects/{project}/topics/{topic}'.format(
         project = pubSubConfig['projectId'],
@@ -14,3 +16,22 @@ def publishMessage(message):
     )
     logger.info(f"publishing message to {psAddress}, message: {message}")
     publisher.publish(psAddress, message)
+
+def serializeJSON(messagePayload):
+    logger.info("serialzing JSON for publishing")
+    epochSeconds = re.search('([0-9]*),', str(messagePayload['t'])) #extract seconds from timestamp object with regex search
+    messagePayload['t'] = epochSeconds.group(1)
+    names_key = {'T' : 'Type' ,                   #key values for JSON to match BQ schema
+                 'S' : 'Symbol',
+                 'o' : 'Open', 
+                 'h' : 'High',
+                 'l' : 'Low',
+                 'c' : 'Close',
+                 'v' : 'Volume',
+                 't' : 'Timestamp', 
+                 'n' : 'numTradeTickers',
+                 'vw': 'VolumeWeightedPrice'
+              }
+    for key in names_key:
+        messagePayload[names_key[key]] = messagePayload.pop(key) #renames keys in JSON to match desired bigQuery output
+    return json.dumps(messagePayload).encode('utf-8') #return JSON object as bytes for publishing to pubsub
